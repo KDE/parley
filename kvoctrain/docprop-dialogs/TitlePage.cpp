f2e9d0a8411629b4c131f28948f9954af5e649e8
Peter Hedlund <peter@peterandlinda.com>
2005-03-10 02:59:18 +0000

    Removed obsolete code
    svn path=/trunk/kdeedu/kvoctrain/; revision=396274

--- kvoctrain/docprop-dialogs/TitlePage.cpp
+++ kvoctrain/docprop-dialogs/TitlePage.cpp
@@ -19,7 +19,7 @@
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   * 
+ *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
 
@@ -45,9 +45,6 @@ TitlePage::TitlePage
   :
   TitlePageForm( parent, name )
 {
-  connect( e_title, SIGNAL(returnPressed()), SLOT(accept()) );
-  connect( e_license, SIGNAL(returnPressed()), SLOT(accept()) );
-
   title = _title;
   author = _author;
   license = _license;
