3f9235f2b0072101bdb8519d617b71e2ea51d120
Stephan Kulow <coolo@kde.org>
2007-02-14 14:25:44 +0000

    ported
    svn path=/branches/work/kconfiggroup_port/kdeedu/kvoctrain/; revision=633602

--- kvoctrain/kva_config.cpp
+++ kvoctrain/kva_config.cpp
@@ -36,7 +36,7 @@
 
 void KVocTrainApp::saveOptions()
 {
-  fileOpenRecent->saveEntries(KGlobal::config().data(), "Recent Files");
+  fileOpenRecent->saveEntries(KGlobal::config()->group( "Recent Files") );
 
   if (m_tableView)
   {
@@ -64,7 +64,7 @@ void KVocTrainApp::saveLanguages()
 
 void KVocTrainApp::readOptions()
 {
-  fileOpenRecent->loadEntries(KGlobal::config().data(), "Recent Files");
+  fileOpenRecent->loadEntries(KGlobal::config()->group( "Recent Files") );
   readLanguages();
 }
 
