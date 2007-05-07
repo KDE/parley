93bf289741373220bdc3125f088f43665a1de3cf
Peter Hedlund <peter@peterandlinda.com>
2007-05-07 22:30:11 +0000

    A final EBN/Krazy fix.
    svn path=/trunk/KDE/kdeedu/kvoctrain/; revision=662346

--- kvoctrain/common-dialogs/kvtlanguageview.h
+++ kvoctrain/common-dialogs/kvtlanguageview.h
@@ -1,14 +1,3 @@
-//
-// C++ Interface: kvtlanguageview
-//
-// Description: 
-//
-//
-// Author: Frederik Gladhorn <frederik.gladhorn@gmx.de>, (C) 2007
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
 /***************************************************************************
 
                              kvtlanguageview
