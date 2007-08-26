816a02d57561c73da9ab40c006918b22d0164ee8
Frederik Gladhorn <gladhorn@kde.org>
2007-08-26 14:10:15 +0000

    fix bracket
    svn path=/trunk/KDE/kdeedu/kvoctrain/; revision=704868

--- kvoctrain/entry-dialogs/EntryDlg.cpp
+++ kvoctrain/entry-dialogs/EntryDlg.cpp
@@ -235,8 +235,8 @@ void EntryDlg::setData(const QList<int>& entries, int currentTranslation)
     m_entries = entries;
     m_currentTranslation = currentTranslation;
 
-    if ( entries.first() < 0 )
-        kDebug() << "EntryDlg::setData() invalid selection: translation: " << currentTranslation << " selection - first row: " << m_entries.first() << " last: " << m_entries.last(); {
+    if ( entries.first() < 0 ) {
+        kDebug() << "EntryDlg::setData() invalid selection: translation: " << currentTranslation << " selection - first row: " << m_entries.first() << " last: " << m_entries.last();
         return;
     }
 
