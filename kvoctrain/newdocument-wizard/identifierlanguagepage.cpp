b026c27521709fd5594cd484c4af147c21cea4e4
Frederik Gladhorn <gladhorn@kde.org>
2007-09-03 18:04:51 +0000

    register identifier fields and make identifier name required
    svn path=/trunk/KDE/kdeedu/kvoctrain/; revision=708045

--- kvoctrain/newdocument-wizard/identifierlanguagepage.cpp
+++ kvoctrain/newdocument-wizard/identifierlanguagepage.cpp
@@ -34,78 +34,6 @@
 
 #include "kvtnewdocumentwizard.h"
 
-// IdentifierLanguagePage::IdentifierLanguagePage(bool isFirstLanguagePage, QWizard *parent)
-//  : QWizardPage(parent)
-// {
-//     m_languageLabel = new QLineEdit;
-//     m_languageShortLabel = new QLineEdit;
-//
-//     m_isFirstLanguagePage = isFirstLanguagePage;
-//
-//     QVBoxLayout *layout = new QVBoxLayout;
-//     if ( isFirstLanguagePage ) {
-//         setTitle(i18n("First Language"));
-//         //setSubTitle(i18n("Now select the first language (e.g. the one you know already)."));
-//
-//         registerField("firstLanguagePage.language", m_languageLabel);
-//         registerField("firstLanguagePage.languageShort", m_languageShortLabel);
-//     } else {
-//         setTitle(i18n("Second Language"));
-//         //setSubTitle(i18n("Now select the language that you want to learn."));
-//         registerField("secondLanguagePage.language", m_languageLabel);
-//         registerField("secondLanguagePage.languageShort", m_languageShortLabel);
-//         m_beforeSelectionLabel = new QLabel;
-//         layout->addWidget(m_beforeSelectionLabel);
-//     }
-//
-//     KVTLanguageView *languageView = new KVTLanguageView(this);
-//     connect(languageView, SIGNAL(signalLanguageChanged(const QString&, const QString&)), this, SLOT(slotLanguageChanged(const QString&, const QString&)));
-//     connect(languageView, SIGNAL(signalActivated()), parent, SLOT(next()));
-//     layout->addWidget(languageView);
-//
-//     m_currentLabel = new QLabel(i18n("Your selection:"));
-//     layout->addWidget(m_currentLabel);
-//
-//     /*
-//     if ( !isFirstLanguagePage ) {*/
-//         /* //More confusing than helping:
-//         QLabel *labelHint = new QLabel(i18n("Please note that you can "
-//             "add more languages later.\n"
-//             "Use Vocabulary->AppendLanguage to do that."));
-//         labelHint->setWordWrap(true);
-//         layout->addWidget(labelHint);*/
-//         /*
-//         m_moreLanguagesCheckbox=new QCheckBox(i18n("I want to add yet another language"));
-//         layout->addWidget(m_moreLanguagesCheckbox);
-//
-//     }*/
-//
-//     //layout->addWidget(m_languageLabel);
-//     //layout->addWidget(m_languageShortLabel);
-//     setLayout(layout);
-// }
-
-/*
-void IdentifierLanguagePage::initializePage()
-{
-kDebug() << "initializePage" << field("firstLanguagePage.language").toString();
-    if (!m_isFirstLanguagePage) {
-        m_beforeSelectionLabel->setText(
-            i18n("So far you have chosen ") +
-            field("firstLanguagePage.language").toString() +
-            i18n("\nPlease add a second language:")
-            );
-    }
-}
-
-
-void IdentifierLanguagePage::slotLanguageChanged(const QString &language, const QString &languageShort)
-{
-    m_languageLabel->setText(language);
-    m_languageShortLabel->setText(languageShort);
-    m_currentLabel->setText(i18n("Your selection: ") + language);
-    kDebug() << "slotLanguageChanged: " << language << " = " << languageShort;
-}*/
 
 /**
  *
@@ -131,6 +59,9 @@ IdentifierLanguagePage::IdentifierLanguagePage(QWizard * parent)
     languageComboBox->setCurrentIndex(current);
 
     connect(languageComboBox, SIGNAL(currentIndexChanged(const QString&)), this, SLOT(currentLanguageChanged(const QString&)));
+
+    registerField("firstIdentifierName*", identifierNameLineEdit);
+    registerField("firstLocale", languageComboBox, "currentText", "editTextChanged(const QString&)");
 }
 
 IdentifierLanguagePage::~IdentifierLanguagePage()
