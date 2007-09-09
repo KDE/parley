325439a12e64a467e856318dd3897c2b7dcf34be
Pino Toscano <pino@kde.org>
2007-09-09 09:12:44 +0000

    kvoctrain.pot -> parley.pot
    svn path=/trunk/KDE/kdeedu/parley/; revision=710198

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 $EXTRACTRC `find . -name "*.ui"` >> rc.cpp || exit 11
-$XGETTEXT `find . -name "*.cpp"` -kI18N_NOOP2 -o $podir/kvoctrain.pot
+$XGETTEXT `find . -name "*.cpp"` -kI18N_NOOP2 -o $podir/parley.pot
