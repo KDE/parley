454aa02fce378ddb5a77bb6cbf0aecc27926b9b5
Frederik Gladhorn <gladhorn@kde.org>
2007-11-17 14:07:55 +0000

    Let configure practice start the practice, saves a few clicks.
    svn path=/trunk/KDE/kdeedu/parley/; revision=737964

--- src/parley.cpp
+++ src/parley.cpp
@@ -623,7 +623,9 @@ void ParleyApp::configurePractice()
     ConfigurePracticeDialog* configurePracticeDialog;
     configurePracticeDialog = new ConfigurePracticeDialog(m_doc, this, "practice settings",  Prefs::self());
 
-    configurePracticeDialog->show();
+    if ( configurePracticeDialog->exec() == KDialog::Accepted ) {
+        startPractice();
+    }
 }
 
 void ParleyApp::startPractice()
