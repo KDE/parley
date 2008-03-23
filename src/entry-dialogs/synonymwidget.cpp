aff241b363e7f12c450bcc71ef99f70479fd2846
Frederik Gladhorn <gladhorn@kde.org>
2008-03-23 11:02:27 +0000

    Get synonym/antonym/false friend right in the gui.
    svn path=/trunk/KDE/kdeedu/parley/; revision=789133

--- src/entry-dialogs/synonymwidget.cpp
+++ src/entry-dialogs/synonymwidget.cpp
@@ -27,15 +27,11 @@ SynonymWidget::SynonymWidget(SynonymWidgetType type, QWidget *parent) : QWidget(
     m_lastTranslation = 0;
     setupUi(this);
 
-//     connect(translationLineEdit, SIGNAL(), SLOT());
     connect(synonymButton, SIGNAL(clicked()), SLOT(togglePair()));
-//     connect(listView, SIGNAL(), SLOT());
-// 
     m_listModel = new QStringListModel(this);     listView->setModel(m_listModel);
 
     setEnabled(false);
 }
-
 
 void SynonymWidget::setTranslation(KEduVocExpression * entry, int translation)
 {
@@ -46,15 +42,7 @@ void SynonymWidget::setTranslation(KEduVocExpression * entry, int translation)
     m_currentTranslation = entry->translation(translation);
 
     if (m_currentTranslation) {
-        synonymButton->setEnabled(true);
         updateList();
-        if (m_lastTranslation) {
-            if (m_currentTranslation->synonyms().contains(m_lastTranslation)) {
-                synonymButton->setText(i18n("%1 and %2 are not Synonyms", m_currentTranslation->text(), m_lastTranslation->text()));
-            } else {
-                synonymButton->setText(i18n("%1 and %2 are Synonyms", m_currentTranslation->text(), m_lastTranslation->text()));
-            }
-        }
     }
 
     if (m_lastTranslation || m_currentTranslation) {
@@ -62,10 +50,47 @@ void SynonymWidget::setTranslation(KEduVocExpression * entry, int translation)
     }
 }
 
-
 void SynonymWidget::updateList()
 {
-    synonymLabel->setText(i18nc("Title for a list of synonyms for a word", "Synonyms of %1:", m_currentTranslation->text()));
+    if (!m_lastTranslation && m_currentTranslation) {
+        synonymButton->setText(i18n("Select a Word"));
+    } else {
+        switch(m_type) {
+            case Synonym:
+                if (m_currentTranslation->synonyms().contains(m_lastTranslation)) {
+                    synonymButton->setText(i18n("%1 and %2 are not Synonyms", m_currentTranslation->text(), m_lastTranslation->text()));
+                } else {
+                    synonymButton->setText(i18n("%1 and %2 are Synonyms", m_currentTranslation->text(), m_lastTranslation->text()));
+                }
+                break;
+            case Antonym:
+                if (m_currentTranslation->antonyms().contains(m_lastTranslation)) {
+                    synonymButton->setText(i18n("%1 and %2 are not Antonyms", m_currentTranslation->text(), m_lastTranslation->text()));
+                } else {
+                    synonymButton->setText(i18n("%1 and %2 are Antonyms", m_currentTranslation->text(), m_lastTranslation->text()));
+                }
+                break;
+            case FalseFriend:
+                if (m_currentTranslation->falseFriends().contains(m_lastTranslation)) {
+                    synonymButton->setText(i18n("%1 and %2 are not False Friends", m_currentTranslation->text(), m_lastTranslation->text()));
+                } else {
+                    synonymButton->setText(i18n("%1 and %2 are False Friends", m_currentTranslation->text(), m_lastTranslation->text()));
+                }
+                break;
+        }
+    }
+
+    switch(m_type) {
+    case Synonym:
+        synonymLabel->setText(i18nc("Title for a list of synonyms for a word", "Synonyms of %1:", m_currentTranslation->text()));
+        break;
+    case Antonym:
+        synonymLabel->setText(i18nc("Title for a list of antonyms (opposites) for a word", "Antonyms of %1:", m_currentTranslation->text()));
+        break;
+    case FalseFriend:
+        synonymLabel->setText(i18nc("Title for a list of false friend (things that sound similiar but have different meanings) for a word", "False Friends of %1:", m_currentTranslation->text()));
+        break;
+    }
 
     // load list of old synonyms
     m_listModel->removeRows(0, m_listModel->rowCount());
@@ -90,18 +115,37 @@ void SynonymWidget::updateList()
 
 void SynonymWidget::togglePair()
 {
-    if (m_currentTranslation->synonyms().contains(m_lastTranslation)) {
-        // break up
-        m_currentTranslation->removeSynonym(m_lastTranslation);
-        m_lastTranslation->removeSynonym(m_currentTranslation);
-
-        updateList();
-    } else {
-        m_currentTranslation->addSynonym(m_lastTranslation);
-        m_lastTranslation->addSynonym(m_currentTranslation);
-
-        updateList();
+    // pair them up
+    switch(m_type) {
+    case Synonym:
+        if (m_currentTranslation->synonyms().contains(m_lastTranslation)) {
+            m_currentTranslation->removeSynonym(m_lastTranslation);
+            m_lastTranslation->removeSynonym(m_currentTranslation);
+        } else {
+            m_currentTranslation->addSynonym(m_lastTranslation);
+            m_lastTranslation->addSynonym(m_currentTranslation);
+        }
+        break;
+    case Antonym:
+        if (m_currentTranslation->antonyms().contains(m_lastTranslation)) {
+            m_currentTranslation->removeAntonym(m_lastTranslation);
+            m_lastTranslation->removeAntonym(m_currentTranslation);
+        } else {
+            m_currentTranslation->addAntonym(m_lastTranslation);
+            m_lastTranslation->addAntonym(m_currentTranslation);
+        }
+        break;
+    case FalseFriend:
+        if (m_currentTranslation->falseFriends().contains(m_lastTranslation)) {
+            m_currentTranslation->removeFalseFriend(m_lastTranslation);
+            m_lastTranslation->removeFalseFriend(m_currentTranslation);
+        } else {
+            m_currentTranslation->addFalseFriend(m_lastTranslation);
+            m_lastTranslation->addFalseFriend(m_currentTranslation);
+        }
+        break;
     }
+    updateList();
 }
 
 
