4692e76e8086ec4b8eac2d40af4d18a14542d1e7
Matt Williams <matt@milliams.com>
2008-04-13 00:20:18 +0000

    Please EBN a tad
    svn path=/trunk/KDE/kdeedu/parley/; revision=796174

--- src/modeltest/modeltest.cpp
+++ src/modeltest/modeltest.cpp
@@ -21,11 +21,11 @@
 **
 ****************************************************************************/
 
+#include "modeltest.h"
+
 #include <QtGui/QtGui>
 
 #include <KDebug>
-
-#include "modeltest.h"
 
 Q_DECLARE_METATYPE(QModelIndex)
 
