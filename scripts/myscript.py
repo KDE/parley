ffe0518c9cb852cb5abdd7a0388ebcb966588697
Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>
2008-06-05 23:40:33 +0000

    fetchTranslation() returns a list of the translated words
    svn path=/branches/work/soc-parley/parley/; revision=817405

--- scripts/myscript.py
+++ scripts/myscript.py
@@ -8,16 +8,13 @@ param_lang_pair = ("langpair","en|fr")
 def fetchTranslation(word):
   param_word_trn = ("q",word)
   request_url = url + "?" + urllib.urlencode([param_word_trn,param_lang_pair])
-  print request_url
+  #print request_url
   results = urllib2.urlopen(request_url)
   data = results.read()
   #p = re.compile('<span\ class="definition">(?:<span class="comment">.*</span>)*([^<>]*)<span class="comment">',re.IGNORECASE)
   p = re.compile('<span\ class="definition">([^<>]*)<span class="comment">',re.IGNORECASE)
-  #l = p.search(data).group(0)
-  #l = p.search(data).group(1)
   l = p.findall(data)
-  print l
-  return "hallo"
+  return l
 
 def returnlist():
     L = [1,2,3,4,5]
