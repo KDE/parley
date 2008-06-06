647181ba37f6660d180e07bca802b3b57bdab560
Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>
2008-06-06 19:40:03 +0000

    extra comments and function order re-arrangement
    svn path=/branches/work/soc-parley/parley/; revision=817732

--- scripts/google_translation.py
+++ scripts/google_translation.py
@@ -3,8 +3,35 @@ import urllib
 import re
 from sgmllib import SGMLParser
 
-url = "http://translate.google.com/translate_dict"
-param_lang_pair = ("langpair","en|fr")
+#fetches the html document for the given word and language pair
+def fetchData(word,from_lang,to_lang):
+  url = "http://translate.google.com/translate_dict"
+  param_word_trn = ("q",word)       #set query parameter
+  param_lang_pair = ("langpair",from_lang+"|"+to_lang)
+  request_url = url + "?" + urllib.urlencode([param_word_trn,param_lang_pair])
+  #print request_url
+  results = urllib2.urlopen(request_url)
+  return results.read()
+
+#parses data and returns the parser object (that contains the translations/langpairs found)
+def parseData(data):
+  p = myParser()
+  p.feed(data)
+  p.close()
+  return p
+
+def fetchTranslation(word,from_lang,to_lang):
+  data = fetchData(word,from_lang,to_lang)
+  parser = parseData(data)
+  return parser.words
+
+def getLanguagePairs():
+  data = fetchData("ignorethis","en","fr")
+  parser = parseData(data)
+  return parser.langpairs
+
+
+# ------------ HTML Parser ----------- #
 
 class myParser(SGMLParser):
   #for every start_tagname function you add you have to make sure the tag is added to the self.tags_stack
@@ -22,7 +49,7 @@ class myParser(SGMLParser):
   def start_span(self, attrs):
     #print "known : ", "span", " ", len(self.tags_stack)
     if ("class","definition") in attrs:
-        self.tags_stack.append("<!translation!>")
+        self.tags_stack.append("<!translation!>") #marks tag with <!translation!> to get its data in handle_data
     else:
         self.tags_stack.append("span")
 
@@ -47,30 +74,3 @@ class myParser(SGMLParser):
   def remove_not_closed_tags(self,tag):
     while len(self.tags_stack) > 0 and self.tags_stack[len(self.tags_stack)-1] != tag:
       self.tags_stack.pop()
-
-#parses data and returns the parser object (that contains the translations/langpairs found)
-def parseData(data):
-  p = myParser()
-  p.feed(data)
-  p.close()
-  return p
-
-def fetchData(word):
-  param_word_trn = ("q",word)       #set query parameter
-  request_url = url + "?" + urllib.urlencode([param_word_trn,param_lang_pair])
-  #print request_url
-  results = urllib2.urlopen(request_url)
-  return results.read()
-  
-def fetchTranslation(word):
-  data = fetchData(word)
-  parser = parseData(data)
-  return parser.words
-
-def getLanguagePairs():
-  data = fetchData("ignorethis")
-  parser = parseData(data)
-  return parser.langpairs
-
-print fetchTranslation("love")
-print getLanguagePairs()
