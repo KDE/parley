91d345ac29aecd02e82787655bfe69dc30fd5c93
Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>
2008-06-06 18:43:58 +0000

    modified to be able to fetch the available language pairs at translate.google.com
    svn path=/branches/work/soc-parley/parley/; revision=817718

--- scripts/google_translation.py
+++ scripts/google_translation.py
@@ -3,27 +3,34 @@ import urllib
 import re
 from sgmllib import SGMLParser
 
-
 url = "http://translate.google.com/translate_dict"
 param_lang_pair = ("langpair","en|fr")
 
 class myParser(SGMLParser):
+  #for every start_tagname function you add you have to make sure the tag is added to the self.tags_stack
 
   def reset(self):
     SGMLParser.reset(self)
-    self.words = []
+    self.words = []         #translated words found in html
+    self.langpairs = []     #language pairs found in html file
     self.tags_stack = []
 
   def unknown_starttag(self,tag,attrs):
         self.tags_stack.append(tag)
         #print "unknown : ", tag, " ", len(self.tags_stack)
-  
+
   def start_span(self, attrs):
     #print "known : ", "span", " ", len(self.tags_stack)
     if ("class","definition") in attrs:
         self.tags_stack.append("<!translation!>")
     else:
         self.tags_stack.append("span")
+
+  def start_option(self, attrs):
+    for name,value in attrs:
+      if name == "value":
+        self.langpairs.append(value)
+    self.tags_stack.append("option")
 
   def handle_data(self,data):
     if self.tags_stack[len(self.tags_stack)-1] == "<!translation!>":
@@ -41,19 +48,29 @@ class myParser(SGMLParser):
     while len(self.tags_stack) > 0 and self.tags_stack[len(self.tags_stack)-1] != tag:
       self.tags_stack.pop()
 
+#parses data and returns the parser object (that contains the translations/langpairs found)
 def parseData(data):
   p = myParser()
   p.feed(data)
   p.close()
-  return p.words
+  return p
 
-def fetchTranslation(word):
-  param_word_trn = ("q",word)
+def fetchData(word):
+  param_word_trn = ("q",word)       #set query parameter
   request_url = url + "?" + urllib.urlencode([param_word_trn,param_lang_pair])
   #print request_url
   results = urllib2.urlopen(request_url)
-  data = results.read()
-  words = parseData(data)
-  return words
+  return results.read()
+  
+def fetchTranslation(word):
+  data = fetchData(word)
+  parser = parseData(data)
+  return parser.words
+
+def getLanguagePairs():
+  data = fetchData("ignorethis")
+  parser = parseData(data)
+  return parser.langpairs
 
 print fetchTranslation("love")
+print getLanguagePairs()
