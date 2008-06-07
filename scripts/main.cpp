a5f157f2c80af83fb7b9d177a3c3513974fb6772
Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>
2008-06-07 00:15:28 +0000

    uses TranslationScript class
    svn path=/branches/work/soc-parley/parley/; revision=817913

--- scripts/main.cpp
+++ scripts/main.cpp
@@ -1,12 +1,16 @@
 #include "scripts.h"
+#include "translationscript.h"
 
 #include <QStringList>
 
 #include <KDebug>
 
 int main() {
-    ScriptTest s;
+    //ScriptTest s;
 
-    kDebug() << QString("Translations: ") << s.translateWord("baby","en","it");
-    s.getLanguagePairs();
+    //kDebug() << QString("Translations: ") << s.translateWord("baby","en","it");
+    //s.getLanguagePairs();
+    TranslationScript script("google_translation.py");
+    script.getLanguagePairs();
+    kDebug() << script.translateWord("car","en","it");
 } 
