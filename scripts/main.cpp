5f19b782195077a1a9bb27bb9638662079a0bf80
Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>
2008-06-07 00:26:14 +0000

    svn path=/branches/work/soc-parley/parley/; revision=817916

--- scripts/main.cpp
+++ scripts/main.cpp
@@ -6,10 +6,6 @@
 #include <KDebug>
 
 int main() {
-    //ScriptTest s;
-
-    //kDebug() << QString("Translations: ") << s.translateWord("baby","en","it");
-    //s.getLanguagePairs();
     TranslationScript script("google_translation.py");
     script.getLanguagePairs();
     kDebug() << script.translateWord("car","en","it");
