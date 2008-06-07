244194a70618a052b9273d94a53583a917b60161
Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>
2008-06-07 00:15:13 +0000

    created TranslationScript class
    svn path=/branches/work/soc-parley/parley/; revision=817912

---
+++ scripts/translationscript.cpp
@@ -0,0 +1,61 @@
+//
+// C++ Implementation: translationscript
+//
+// Description: 
+//
+//
+// Author: Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>, (C) 2008
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include "translationscript.h"
+
+#include <KDebug>
+#include <QVariant>
+
+TranslationScript::TranslationScript(QString filename)
+{
+    action = new Kross::Action(this,"Action");
+    action->setFile(filename);
+}
+
+
+TranslationScript::~TranslationScript()
+{
+}
+
+/*!
+    \fn ScriptTest::getLanguagePairs()
+ */
+void TranslationScript::getLanguagePairs()
+{
+    QVariant script_result = action->callFunction("getLanguagePairs");
+
+    foreach(QVariant langpair, script_result.toList()) {
+        QVariantList pair = langpair.toList();
+        QString from = pair[0].toString();
+        QString to = pair[1].toString();
+        kDebug() << from << " -> " << to;
+    }
+}
+
+/*!
+    \fn ScriptTest::translateWord(QString Word)
+ */
+QStringList TranslationScript::translateWord(QString word, QString from, QString to)
+{
+    Kross::Action action(this,"MyScript");
+    action.setFile("google_translation.py");
+    QVariantList args;
+    args << word << from << to;
+    QVariant script_result = action.callFunction("fetchTranslation",args);
+    QList<QVariant> translations = script_result.toList();
+
+    QStringList result;
+
+    for (int i = 0; i < translations.size(); i++)
+        result.push_back(translations[i].toString());
+
+    return result;
+}
