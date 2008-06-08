204b32fcdfbd8a98d9a144bf38e26aaf22244877
Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>
2008-06-08 01:06:18 +0000

    testing the ectaco string
    svn path=/branches/work/soc-parley/parley/; revision=818180

--- scripts/main.cpp
+++ scripts/main.cpp
@@ -2,11 +2,20 @@
 #include "translationscript.h"
 
 #include <QStringList>
+#include <QTextCodec>
 
 #include <KDebug>
 
 int main() {
-    TranslationScript script("google_translation.py");
-    script.getLanguagePairs();
-    kDebug() << script.translateWord("car","en","it");
+    QTextCodec* Utf8Codec  = QTextCodec::codecForName("utf-8");
+
+    //TranslationScript script("google_translation.py");
+    //script.getLanguagePairs();
+    //kDebug() << script.translateWord("car","en","it");
+
+    TranslationScript s("ectaco.py");
+    foreach(QString word, s.translateWord("test","en","pl"))
+        kDebug() << "trns: " << word;
+
+    return 0;
 } 
