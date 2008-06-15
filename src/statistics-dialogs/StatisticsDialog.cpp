419afaa4154aa88bce150d4347504fe2a43a94a1
Frederik Gladhorn <gladhorn@kde.org>
2008-06-15 15:02:32 +0000

    clean up statistics dialog
    svn path=/trunk/KDE/kdeedu/parley/; revision=820835

--- src/statistics-dialogs/StatisticsDialog.cpp
+++ src/statistics-dialogs/StatisticsDialog.cpp
@@ -1,15 +1,6 @@
 /***************************************************************************
 
-                      statistics dialog class
-
-    -----------------------------------------------------------------------
-
-    begin         : Sun Sep 19 20:50:53 MET 1999
-
-    copyright     : (C) 1999-2001 Ewald Arnold <kvoctrain@ewald-arnold.de>
-                    (C) 2005-2007 Peter Hedlund <peter.hedlund@kdemail.net>
-
-    -----------------------------------------------------------------------
+    Copyright 2008 Frederik Gladhorn <frederik.gladhorn@kdemail.net>
 
  ***************************************************************************
 
@@ -28,45 +19,21 @@
 #include <KConfig>
 #include <KGlobal>
 
-#include "StatisticsPage.h"
-#include "generalstatisticspage.h"
 #include "lessonstatistics.h"
 #include "keduvocdocument.h"
-
 #include "statisticsmodel.h"
 
-#include "modeltest/modeltest.h"
-
-KVTStatisticsDialog::KVTStatisticsDialog(KEduVocDocument *doc, QWidget *parent) : KPageDialog(parent), m_doc(doc)
+KVTStatisticsDialog::KVTStatisticsDialog(KEduVocDocument *doc, QWidget *parent) : KDialog(parent), m_doc(doc)
 {
     setCaption(i18n("Document Statistics"));
-    setButtons(Ok | Apply | Cancel);
-    setDefaultButton(Ok);
+    setButtons(Ok);
     setModal(true);
-    setFaceType(KPageDialog::Tabbed);
-
-    StatisticsPage *spage;
-
-    GeneralStatisticsPage *gspage = new GeneralStatisticsPage(doc, this);
-    addPage(gspage, i18nc("general statistics page", "General"));
-
-/*
-    for (int i = 1; i < (int) doc->identifierCount(); i++) {
-        QString s;
-        s = doc->identifier(i).name();
-
-        spage = new StatisticsPage(i, doc, this);
-        addPage(spage, s);
-        pageList.append(spage);
-    }
-*/
-    connect(this, SIGNAL(applyClicked()), this, SLOT(slotApply()));
 
     StatisticsModel *statisticsModel = new StatisticsModel(this);
     statisticsModel->setDocument(doc);
 
     LessonStatisticsView *lessonStatistics = new LessonStatisticsView(this);
-    addPage(lessonStatistics, i18n("Lesson Statistics"));
+    setMainWidget(lessonStatistics);
 
     lessonStatistics->setModel(statisticsModel);
     lessonStatistics->expandToDepth(0);
@@ -79,21 +46,6 @@ KVTStatisticsDialog::~KVTStatisticsDialog()
 {
     KConfigGroup cg(KGlobal::config(), "StatisticsDialog");
     KDialog::saveDialogSize(cg);
-}
-
-void KVTStatisticsDialog::slotApply()
-{
-    foreach(StatisticsPage *page, pageList) {
-        page->resetStatistics();
-    }
-    //m_doc->lesson()->resetGrades();
-}
-
-
-void KVTStatisticsDialog::accept()
-{
-    slotApply();
-    KPageDialog::accept();
 }
 
 #include "StatisticsDialog.moc"
