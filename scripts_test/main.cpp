07972bbbb401d94b09024bd316be0774d8abb0f0
Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>
2008-06-16 23:37:11 +0000

    renamed scripts test folder to scripts_test
    svn path=/branches/work/soc-parley/parley/; revision=821265

---
+++ scripts_test/main.cpp
@@ -0,0 +1,22 @@
+#include "scripts.h"
+#include "translationscript.h"
+
+#include <QStringList>
+#include <QTextCodec>
+
+#include <KDebug>
+
+int main() {
+    QTextCodec* Utf8Codec  = QTextCodec::codecForName("utf-8");
+
+    //TranslationScript script("google_translation.py");
+    //script.getLanguagePairs();
+    //kDebug() << script.translateWord("car","en","it");
+
+    TranslationScript s("ectaco.py");
+//     kDebug() << s.translateWord("test","en","pl");
+    foreach(QString word, s.translateWord("test","en","pl"))
+        kDebug() << "trns: " << word;
+
+    return 0;
+} 
