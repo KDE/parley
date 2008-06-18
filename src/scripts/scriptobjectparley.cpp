18c29f3f5ab8b41ca1e4940ed64795d6e8c3cc14
Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>
2008-06-18 11:45:26 +0000

    adding ScriptObjectParley class
    svn path=/branches/work/soc-parley/parley/; revision=821758

---
+++ src/scripts/scriptobjectparley.cpp
@@ -0,0 +1,23 @@
+//
+// C++ Implementation: scriptobjectparley
+//
+// Description: 
+//
+//
+// Author: Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>, (C) 2008
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include "scriptobjectparley.h"
+
+ScriptObjectParley::ScriptObjectParley()
+{
+}
+
+
+ScriptObjectParley::~ScriptObjectParley()
+{
+}
+
+
