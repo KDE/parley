f39aa0d3ad22cc89999fb62146032c6be81f05bc
Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>
2008-06-18 11:49:23 +0000

    svn path=/branches/work/soc-parley/parley/; revision=821762

---
+++ src/scripts/scriptobjectparley.h
@@ -0,0 +1,28 @@
+//
+// C++ Interface: scriptobjectparley
+//
+// Description: 
+//
+//
+// Author: Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>, (C) 2008
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef SCRIPTOBJECTPARLEY_H
+#define SCRIPTOBJECTPARLEY_H
+
+/**
+Implements the object that will be given to Kross scripts to use for accessing parley features
+
+	@author Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>
+*/
+class ScriptObjectParley{
+public:
+    ScriptObjectParley();
+
+    ~ScriptObjectParley();
+
+};
+
+#endif
