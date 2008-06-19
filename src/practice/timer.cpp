1f59c6bf12b3441f691b97fc22c89fa9d39ee0c8
David Capel <wot.narg@gmail.com>
2008-06-19 09:08:04 +0000

    adding a file
    svn path=/branches/work/soc-parley/parley/; revision=822116

---
+++ src/practice/timer.cpp
@@ -0,0 +1,54 @@
+//
+// C++ Implementation: timer
+//
+// Description:
+//
+//
+// Author: David Capel <wot.narg@gmail.com>, (C) 2008
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+#include "timer.h"
+
+#include <KDebug>
+
+InvisibleTimer::InvisibleTimer(QObject * parent)
+    : QObject(parent)
+{
+    kDebug() << "ctor'ed";
+    m_timer = new QTimer();
+    connect(m_timer, SIGNAL(timeout()), this, SLOT(slotLog()));
+}
+
+InvisibleTimer::~InvisibleTimer()
+{
+    delete m_timer;
+}
+
+void InvisibleTimer::slotStart(Milliseconds ms)
+{
+    kDebug() << "hit with " << ms;
+    setLength(ms);
+    slotStart();
+}
+
+void InvisibleTimer::slotStart()
+{
+    kDebug() << "...continues with " << m_length;
+    m_timer->setSingleShot(true);
+    m_timer->start(m_length);
+}
+
+void InvisibleTimer::slotStop()
+{
+    kDebug() << "stopped";
+    m_timer->stop();
+}
+
+void InvisibleTimer::slotLog()
+{
+    kDebug() << "hit";
+    emit signalTimeout();
+}
