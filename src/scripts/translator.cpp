ce3320933426a9ba612486a6f306edc3a96acc01
Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>
2008-07-01 10:52:37 +0000

    Translator class added
    svn path=/branches/work/soc-parley/parley/; revision=826768

---
+++ src/scripts/translator.cpp
@@ -0,0 +1,23 @@
+//
+// C++ Implementation: translator
+//
+// Description: 
+//
+//
+// Author: Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>, (C) 2008
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include "translator.h"
+
+Translator::Translator()
+{
+}
+
+
+Translator::~Translator()
+{
+}
+
+
---
+++ src/scripts/translator.h
@@ -0,0 +1,28 @@
+//
+// C++ Interface: translator
+//
+// Description: 
+//
+//
+// Author: Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>, (C) 2008
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef TRANSLATOR_H
+#define TRANSLATOR_H
+
+/**
+Keeps the translated words
+
+	@author Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>
+*/
+class Translator{
+public:
+    Translator();
+
+    ~Translator();
+
+};
+
+#endif
