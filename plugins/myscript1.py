3aab005ba51ca5b6ee199ad1829e93c026e5d7bb
Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>
2008-07-09 08:16:49 +0000

    svn path=/branches/work/soc-parley/parley/; revision=829814

--- plugins/myscript1.py
+++ plugins/myscript1.py
@@ -9,7 +9,10 @@ def translateWord(word,fromLang,toLang):
   print "myscript1: Translating Word ...", word, fromLang, toLang
   Parley.addTranslation("Hello","en_US","fr","bonbon")
 
-print "myscript1"
+print Parley.languageCodes()
+print Parley.languageCodeToName("en_US")
+
+#print "myscript1"
 #init()
 #Parley.callFromScript()
 #Parley.connect("signalTranslateWord(QString)",translate)
