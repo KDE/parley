c83f0178da78d53918cb1713a6cda8a8c1f45cf8
Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>
2008-07-18 09:51:39 +0000

    changed Licence documentation
    svn path=/branches/work/soc-parley/parley/; revision=834163

--- src/scripts/scriptdialog.cpp
+++ src/scripts/scriptdialog.cpp
@@ -1,14 +1,17 @@
-//
-// C++ Implementation: scriptdialog
-//
-// Description:
-//
-//
-// Author: Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>, (C) 2008
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
+/***************************************************************************
+
+    Copyright 2008 Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 #include "scriptdialog.h"
 
 #include "scripting/parley.h"
--- src/scripts/scripting/text.h
+++ src/scripts/scripting/text.h
@@ -1,14 +1,17 @@
-//
-// C++ Interface: text
-//
-// Description:
-//
-//
-// Author: Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>, (C) 2008
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
+/***************************************************************************
+
+    Copyright 2008 Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 #ifndef SCRIPTINGTEXT_H
 #define SCRIPTINGTEXT_H
 
