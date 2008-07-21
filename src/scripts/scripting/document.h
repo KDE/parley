84a842dbe04acacbcc701c95e1649168f817ba5d
Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>
2008-07-21 13:30:24 +0000

    Minor comment changes (ignore)
    svn path=/branches/work/soc-parley/parley/; revision=835974

--- src/scripts/scripting/document.h
+++ src/scripts/scripting/document.h
@@ -40,13 +40,7 @@ namespace Scripting
     class Document : public QObject
     {
             Q_OBJECT
-
-            /** Read-Only property that gives the root of the lesson tree
-            * @code
-            * import Parley
-            * root = Parley.document.rootLesson
-            * @endcode
-            */
+            /// Document's root lesson
             Q_PROPERTY ( QObject * rootLesson READ rootLesson )
         public:
             Document ( KEduVocDocument * doc );
@@ -87,13 +81,15 @@ namespace Scripting
              */
             QVariantList allLessons();
 
+            /**
+             * Sets the word type (@p wordtype) of the given @p tr translation object.
+            *  If the @p wordtype is not valid, no changes are made to the translation object
+             * @param tr Translation object to set it's word type
+             * @param wordtype Word type name
+             */
             void setWordType ( QObject * tr, const QString & wordtype );
 
             QStringList wordTypes();
-
-            // *** tense methods ***
-
-
 
 // ---------------------  copied from KEduVocDocument
 
@@ -131,7 +127,7 @@ namespace Scripting
              *
              * @param dirty   new state
              */
-            void setModified ( bool dirty = true ) { m_doc->setModified(dirty); }
+            void setModified ( bool dirty = true ) { m_doc->setModified ( dirty ); }
 
             /** @returns the modification state of the doc */
             bool isModified() const { return m_doc->isModified(); }
@@ -147,42 +143,42 @@ namespace Scripting
 
             /** set the title of the file
              * @param title title to set */
-            void setTitle ( const QString & title ) { m_doc->setTitle(title); }
+            void setTitle ( const QString & title ) { m_doc->setTitle ( title ); }
 
             /** @returns the title of the file */
             QString title() const { return m_doc->title(); }
 
             /** set the author of the file
              * @param author author to set */
-            void setAuthor ( const QString & author ) { m_doc->setAuthor(author); }
+            void setAuthor ( const QString & author ) { m_doc->setAuthor ( author ); }
 
             /** @returns the author of the file */
             QString author() const { return m_doc->author(); }
 
             /** set the author contact info
              * @param contact email/contact info to set */
-            void setAuthorContact ( const QString & authorContact ) { m_doc->setAuthorContact(authorContact); }
+            void setAuthorContact ( const QString & authorContact ) { m_doc->setAuthorContact ( authorContact ); }
 
             /** @returns the author contact information */
             QString authorContact() const { return m_doc->authorContact(); }
 
             /** set the license of the file
              * @param license license to set */
-            void setLicense ( const QString & license ) { m_doc->setLicense(license); }
+            void setLicense ( const QString & license ) { m_doc->setLicense ( license ); }
 
             /** @returns the license of the file */
             QString license() const { return m_doc->license(); }
 
             /** set the comment of the file
              * @param comment comment to set */
-            void setDocumentComment ( const QString & comment ) { m_doc->setDocumentComment(comment); }
+            void setDocumentComment ( const QString & comment ) { m_doc->setDocumentComment ( comment ); }
 
             /** @return the comment of the file */
             QString documentComment() const { return m_doc->documentComment(); }
 
             /** set the category of the file
              * @param category category to set */
-            void setCategory ( const QString & category ) { m_doc->setCategory(category); }
+            void setCategory ( const QString & category ) { m_doc->setCategory ( category ); }
 
             /** @return the category of the file */
             QString category() const { return m_doc->category(); }
@@ -190,14 +186,14 @@ namespace Scripting
             /**
              * Sets the generator of the file
              */
-            void setGenerator ( const QString & generator ) { m_doc->setGenerator(generator); }
+            void setGenerator ( const QString & generator ) { m_doc->setGenerator ( generator ); }
 
             /** @returns the generator of the file */
             QString generator() const { m_doc->generator(); }
 
             /** Sets version of the loaded file
              * @param ver the new version */
-            void setVersion ( const QString & ver ) { m_doc->setVersion(ver); }
+            void setVersion ( const QString & ver ) { m_doc->setVersion ( ver ); }
 
             /** @returns the version of the loaded file */
             QString version() const { return m_doc->version(); }
@@ -238,7 +234,7 @@ namespace Scripting
              *
              * @param index            number of translation 0..x
              */
-            void removeIdentifier ( int index ) { m_doc->removeIdentifier(index); }
+            void removeIdentifier ( int index ) { m_doc->removeIdentifier ( index ); }
 
             /**
              * Determines the index of a given identifier
@@ -321,7 +317,7 @@ namespace Scripting
              *
              * @param delimiter        the delimiter to use
              */
-            void setCsvDelimiter ( const QString &delimiter ) { m_doc->setCsvDelimiter(delimiter); }
+            void setCsvDelimiter ( const QString &delimiter ) { m_doc->setCsvDelimiter ( delimiter ); }
 
 //             static FileType detectFileType ( const QString &fileName );
 
