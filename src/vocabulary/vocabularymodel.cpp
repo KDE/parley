788e3dee0570b3eaf1ec461d69b57af7cb17b4d8
Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>
2008-07-22 11:46:16 +0000

    Changes to google dictionary translation to reflect some changes made by Google
    svn path=/branches/work/soc-parley/parley/; revision=836493

--- src/vocabulary/vocabularymodel.cpp
+++ src/vocabulary/vocabularymodel.cpp
@@ -253,7 +253,7 @@ bool VocabularyModel::setData(const QModelIndex &index, const QVariant &value, i
     }
 
     emit(dataChanged(index, index));
-    m_document->setModified();
+//     m_document->setModified();
     return true;
 }
 
