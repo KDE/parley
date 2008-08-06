9e4abfc16d219f3e37f4bfe9b4861d5c3a24d3f0
David Capel <wot.narg@gmail.com>
2008-08-06 09:50:30 +0000

    Adding a license to new files.
    svn path=/trunk/KDE/kdeedu/parley/; revision=842945

--- src/practice/multiple/multiplewidgetparent.cpp
+++ src/practice/multiple/multiplewidgetparent.cpp
@@ -6,9 +6,15 @@
 //
 // Author: David Capel <wot.narg@gmail.com>, (C) 2008
 //
-// Copyright: See COPYING file that comes with this distribution
-//
-//
+/***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************/
+
 
 #include "multiplewidgetparent.h"
 
