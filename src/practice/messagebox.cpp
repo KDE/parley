c1d5251e9d332c30b6feff10b5b69cb5749b2732
David Capel <wot.narg@gmail.com>
2008-08-15 14:57:53 +0000

    Adding messagebox files
    svn path=/trunk/KDE/kdeedu/parley/; revision=847524

---
+++ src/practice/messagebox.cpp
@@ -0,0 +1,45 @@
+//
+// C++ Implementation: messagebox
+//
+// Description:
+//
+//
+// Author: David Capel <wot.narg@gmail.com>, (C) 2008
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+/***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************/
+
+#include "messagebox.h"
+#include "activearea.h"
+
+#include <KSvgRenderer>
+#include <QString>
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <KDebug>
+
+MessageBox::MessageBox ( KSvgRenderer * renderer, ActiveArea * area, const QString& elementId, const QString& text, QGraphicsItem * parent ) :
+        QGraphicsTextItem(parent), m_renderer ( renderer ), m_area(area)
+{
+    QString tId = area->translateElementId(elementId);
+    if (tId.isEmpty()) setVisible(false);
+
+    m_backgroundRect = renderer->boundsOnElement ( tId );
+    setPos (area->offset().x() + m_backgroundRect.x()/* + m_backgroundRect.width() / 20.0*/, area->offset().y() + m_backgroundRect.y() + m_backgroundRect.height() / 4.0 );
+    adjustSize();
+    setZValue(5);
+
+    setTextWidth(m_backgroundRect.width()*.8);
+
+    setHtml("<center>" + text + "</center>");
+}
---
+++ src/practice/messagebox.h
@@ -0,0 +1,48 @@
+//
+// C++ Interface: messagebox
+//
+// Description:
+//
+//
+// Author: David Capel <wot.narg@gmail.com>, (C) 2008
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+/***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************/
+
+#ifndef MESSAGEBOX_H
+#define MESSAGEBOX_H
+
+#include <QGraphicsTextItem>
+
+class KSvgRenderer;
+class QString;
+class QGraphicsView;
+class QWidget;
+class QRectF;
+class ActiveArea;
+
+/// @class MessageBox
+/// Used to display static messages to the user; all input to @param text should be passed through i18n()
+class MessageBox : public QGraphicsTextItem
+{
+        Q_OBJECT
+
+    public:
+        MessageBox(KSvgRenderer * renderer, ActiveArea * area,  const QString& elementId, const QString& text, QGraphicsItem * parent = 0);
+    private:
+        KSvgRenderer * m_renderer;
+        QRectF m_backgroundRect;
+        ActiveArea* m_area;
+};
+
+#endif
