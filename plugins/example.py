f42c2e0c888538fc876deffee26256ad8100bb7e
Frederik Gladhorn <gladhorn@kde.org>
2008-11-21 12:23:16 +0000

    fix string
    svn path=/trunk/KDE/kdeedu/parley/; revision=887188

--- plugins/example.py
+++ plugins/example.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env kross
+# -*- coding: utf-8 -*-
 import Parley
 
 #FUNCTIONS
@@ -20,11 +21,11 @@ def markAsKnown():
 
 #create a new action for the Scripts menu (action1)
 action1 = Parley.newAction("example_action1","Move to new lesson")
-action1.statusTip="Moves the selected rows to a new lesson"
+action1.statusTip="Moves the selected vocabulary to a new lesson"
 Parley.connect(action1,"triggered()",moveSelectedToNewLesson)
 
 
 #create a new action for the Scripts menu (action2)
 action2 = Parley.newAction("example_action2","Mark as known (highest grade)")
-action2.statusTip="Sets the grade selected translations to 7 (highest grade)"
+action2.statusTip="Sets the grade for the selected words to 7 (highest grade)"
 Parley.connect(action2,"triggered()",markAsKnown)
