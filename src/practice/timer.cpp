47f51957c3dd1702263fd1edc1c5736feab382fc
David Capel <wot.narg@gmail.com>
2009-01-01 03:38:35 +0000

    Adding a license and copyright notice -- they were missing due to an oversight from before.
    svn path=/trunk/KDE/kdeedu/parley/; revision=903993

--- src/practice/timer.cpp
+++ src/practice/timer.cpp
@@ -1,12 +1,17 @@
-//
-// C++ Implementation: timer
-//
-// Description:
-//
-//
-// Copyright 2008 David Capel <wot.narg@gmail.com>
-//
-//
+/***************************************************************************
+
+    Copyright 2008 David Capel <wot.narg@gmail.com>
+
+ ***************************************************************************
+
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 #include "timer.h"
 #include "activearea.h"
