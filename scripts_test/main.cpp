d954734607e4013011b560f8c2dde49803ae0f52
Eckhart Wörner <ewoerner@kde.org>
2009-01-08 22:08:23 +0000

    krazy: Check for an acceptable license (3 issues) CCMAIL: avgoustinos.kadis@kdemail.net
    svn path=/trunk/KDE/kdeedu/parley/; revision=907928

--- scripts_test/main.cpp
+++ scripts_test/main.cpp
@@ -1,3 +1,18 @@
+/***************************************************************************
+
+    Copyright 2008 Avgoustinos Kadis <avgoustinos.kadis@kdemail.net>
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "scripts.h"
 #include "translationscript.h"
 
