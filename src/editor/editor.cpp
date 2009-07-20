KDE/parley

commit a0542382fa59e96543de7a79bf4dd2c9f73f8cd3

fix crash when opening a document after closing another one. this is not the best fix possible but will do for 4.3
svn path=/trunk/KDE/kdeedu/parley/; revision=999405

src/editor/editor.cpp
@@ -92,6 +92,7 @@ void Editor::updateDocument()
 {
 ///@todo we can use connect here
     m_vocabularyModel->setDocument(m_mainWindow->parleyDocument()->document());
+    m_vocabularyView->setDocument(m_mainWindow->parleyDocument()->document());
 
     m_lessonModel->setDocument(m_mainWindow->parleyDocument()->document());
     m_wordTypeModel->setDocument(m_mainWindow->parleyDocument()->document());

commit 0ab0ea67575cff5ca40fc2c79962ebf7b567a85c

fix crash introduced by r999405
the view has to get the new document before the model, as the reset of the model causes the
view to access the document

CCMAIL:gladhorn@kde.org

svn path=/trunk/KDE/kdeedu/parley/; revision=1000164

src/editor/editor.cpp
@@ -91,8 +91,8 @@ Editor::~Editor()
 void Editor::updateDocument()
 {
 ///@todo we can use connect here
-    m_vocabularyModel->setDocument(m_mainWindow->parleyDocument()->document());
     m_vocabularyView->setDocument(m_mainWindow->parleyDocument()->document());
+    m_vocabularyModel->setDocument(m_mainWindow->parleyDocument()->document());
 
     m_lessonModel->setDocument(m_mainWindow->parleyDocument()->document());
     m_wordTypeModel->setDocument(m_mainWindow->parleyDocument()->document());