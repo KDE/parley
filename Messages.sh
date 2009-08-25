7eea0550e3030b8e8768a02b9e14f1daff56fc13
Frederik Gladhorn <gladhorn@kde.org>
2009-08-25 14:47:06 +0000

    start extracting messages from py files
    svn path=/trunk/KDE/kdeedu/parley/; revision=1015458

--- Messages.sh
+++ Messages.sh
@@ -7,5 +7,7 @@ $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
 # call xgettext on all source files. If your sources have other filename
 # extensions besides .cc, .cpp, and .h, just add them in the find call.
 $XGETTEXT `find . -name \*.cpp -o -name \*.h` -o $podir/parley.pot
+$XGETTEXT `find . -name \*.py` -o $podir/parley.pot
 # remove the generated files
 rm -f tips.cpp rc.cpp
+
