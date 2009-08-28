5bb87067699ebd89b74537033de7ac88afa17992
Chusslove Illich <caslav.ilic@gmx.net>
2009-08-28 20:12:34 +0000

    Extract second time using -j option to join with previous POT. (Removes some artifacts after msgcat.)
    svn path=/trunk/KDE/kdeedu/parley/; revision=1016762

--- Messages.sh
+++ Messages.sh
@@ -7,8 +7,7 @@ $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
 # call xgettext on all source files. If your sources have other filename
 # extensions besides .cc, .cpp, and .h, just add them in the find call.
 $XGETTEXT `find . -name \*.cpp -o -name \*.h` -o $podir/parley.pot
-$XGETTEXT -L python `find . -name \*.py` -o python_parley.pot
-msgcat $podir/parley.pot python_parley.pot -o $podir/parley.pot
+$XGETTEXT -L python `find . -name \*.py` -o $podir/parley.pot -j
 # remove the generated files
 rm -f tips.cpp rc.cpp python_parley.pot
 
