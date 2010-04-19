c51c1076ef588424c02864d95cb278c81a509d67
Daniel Laidig <laidig@kde.org>
2010-04-19 15:57:44 +0000

    oops, there was still a template license header...
    svn path=/trunk/KDE/kdeedu/parley/; revision=1116492

--- src/practice/writtenpracticewidget.cpp
+++ src/practice/writtenpracticewidget.cpp
@@ -1,22 +1,16 @@
-/*
-    <one line to give the program's name and a brief idea of what it does.>
-    Copyright (C) <year>  <name of author>
+/***************************************************************************
+    Copyright 2009-2010 Frederik Gladhorn <gladhorn@kde.org>
+    Copyright 2009-2010 Daniel Laidig <laidig@kde.org>
+ ***************************************************************************/
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-*/
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 #include "writtenpracticewidget.h"
 #include "ui_practice_widget_written.h"
