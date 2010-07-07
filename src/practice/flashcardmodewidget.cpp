c6710b25454a3a9ba4dee64cfa365d0066ca1f6a
Daniel Laidig <laidig@kde.org>
2010-07-07 19:26:14 +0000

    make sure to set the correct palette before starting the latex rendering
    it's green!
    
    svn path=/branches/KDE/4.5/kdeedu/parley/; revision=1147271

--- src/practice/flashcardmodewidget.cpp
+++ src/practice/flashcardmodewidget.cpp
@@ -66,6 +66,7 @@ void FlashCardModeWidget::showSynonym()
 void FlashCardModeWidget::showSolution()
 {
     m_ui->solutionLabel->setHidden(false);
+    m_ui->solutionLabel->setPalette(m_correctPalette);
 
     m_ui->solutionLabel->setMinimumSize(QSize(0, 0));
     if (LatexRenderer::isLatex(m_solution)) {
@@ -78,7 +79,6 @@ void FlashCardModeWidget::showSolution()
         m_ui->solutionLabel->setText(m_solution);
     }
 
-    m_ui->solutionLabel->setPalette(m_correctPalette);
     m_frontend->showSetResultButtons(true);
 }
 
