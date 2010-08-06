a6b19b2ae5bc01948543125612704bbd5fdf0f37
Frederik Gladhorn <gladhorn@kde.org>
2010-08-06 14:12:49 +0000

    Flash card mode: change the solution text before making it visible, otherwise for a short moment the last solution is visible.
    svn path=/trunk/KDE/kdeedu/parley/; revision=1159914

--- src/practice/flashcardmodewidget.cpp
+++ src/practice/flashcardmodewidget.cpp
@@ -65,7 +65,6 @@ void FlashCardModeWidget::showSynonym()
 
 void FlashCardModeWidget::showSolution()
 {
-    m_ui->solutionLabel->setHidden(false);
     m_ui->solutionLabel->setPalette(m_correctPalette);
 
     m_ui->solutionLabel->setMinimumSize(QSize(0, 0));
@@ -79,6 +78,7 @@ void FlashCardModeWidget::showSolution()
         m_ui->solutionLabel->setText(m_solution);
     }
 
+    m_ui->solutionLabel->setHidden(false);
     m_frontend->showSetResultButtons(true);
 }
 
