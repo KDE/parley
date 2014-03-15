cf712f4767efd336958178bae3d979eeac317347
Inge Wallin <inge@lysator.liu.se>
2014-03-15 23:37:16 +0100

    Create a dialog for general practice session settings.

---
+++ src/configure-practice/generalpracticeoptions.cpp
@@ -0,0 +1,38 @@
+/***************************************************************************
+
+                      query options for Parley
+
+    -----------------------------------------------------------------------
+
+    begin                : Tue Apr 5 2005
+
+    copyright            :(C) 2005 Peter Hedlund <peter.hedlund@kdemail.net>
+                          (C) 2007 Frederik Gladhorn <frederik.gladhorn@kdemail.net>
+                          (C) 2014 Inge Wallin <inge@lysator.liu.se>
+
+    -----------------------------------------------------------------------
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "generalpracticeoptions.h"
+
+#include "prefs.h"
+
+GeneralPracticeOptions::GeneralPracticeOptions(QWidget* parent) : QWidget(parent)
+{
+    setupUi(this);
+
+    //connect(kcfg_ShowSolutionAfterAnswer, SIGNAL(toggled(bool)), kcfg_ShowSolutionAfterAnswerTime, SLOT(setEnabled(bool)));
+}
+
+
+#include "generalpracticeoptions.moc"
---
+++ src/configure-practice/generalpracticeoptions.h
@@ -0,0 +1,38 @@
+/***************************************************************************
+
+                      query options for Parley
+
+    -----------------------------------------------------------------------
+
+    begin                : Tue Apr 5 2005
+
+    copyright            :(C) 2005 Peter Hedlund <peter.hedlund@kdemail.net>
+                          (C) 2007 Frederik Gladhorn <frederik.gladhorn@kdemail.net>
+                          (C) 2014 Inge Wallin <inge@lysator.liu.se>
+
+    -----------------------------------------------------------------------
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef GENERALPRACTICEOPTIONS_H
+#define GENERALPRACTICEOPTIONS_H
+
+#include "ui_generalpracticeoptions.h"
+
+class GeneralPracticeOptions : public QWidget, public Ui::GeneralPracticeOptions
+{
+    Q_OBJECT
+public:
+    GeneralPracticeOptions(QWidget* parent = 0);
+};
+
+#endif
