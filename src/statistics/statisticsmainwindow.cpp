8481cb0f38c3b32bdbc17bc045a87abcfb3090e3
Inge Wallin <inge@lysator.liu.se>
2014-04-06 10:29:16 +0200

    Fix ugly caption in the statistics window.

--- src/statistics/statisticsmainwindow.cpp
+++ src/statistics/statisticsmainwindow.cpp
@@ -53,7 +53,8 @@ StatisticsMainWindow::StatisticsMainWindow(KEduVocDocument* doc, ParleyMainWindo
     setCentralWidget(mainWidget);
     m_ui = new Ui::StatisticsMainWindow;
     m_ui->setupUi(mainWidget);
-    m_ui->caption->setText(i18nc("caption for an overview of the grades for a document", "Statistics for \"%1\"", m_doc->title()));
+    //m_ui->caption->setText(i18nc("caption for an overview of the grades for a document"
+    //                             "Statistics for \"%1\"", m_doc->title()));
     m_statisticsModel = new StatisticsModel(this);
 
     setDocument(doc);
