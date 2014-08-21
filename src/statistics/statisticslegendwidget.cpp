0df9961e88e56c2ec084f97bef5445ca665f260a
Inge Wallin <inge@lysator.liu.se>
2014-08-22 01:39:44 +0700

    Make some code follow the coding standards.

--- src/statistics/statisticslegendwidget.cpp
+++ src/statistics/statisticslegendwidget.cpp
@@ -19,82 +19,95 @@
  *  License along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+
+// Own
 #include "statisticslegendwidget.h"
-#include <keduvocexpression.h>
-#include "prefs.h"
 
-#include <KLocalizedString>
-
+// Qt
 #include <QDebug>
 #include <QPainter> ///
 
+// KDE
+#include <KLocalizedString>
+
+// Parley
+#include <keduvocexpression.h>
+#include "prefs.h"
+
+
 StatisticsLegendWidget::StatisticsLegendWidget(QWidget * parent)
-    : QWidget( parent )
+    : QWidget(parent)
 {
     QString ttip;
-    ttip += "<table><tr><td>"+i18n( "Progress gradients" )+"</td><td bgcolor=\""
+    ttip += "<table><tr><td>"+i18n("Progress gradients")+"</td><td bgcolor=\""
             + gradeColor(KV_MAX_GRADE - 1).name()+"\"><nobr>    </nobr></td></tr>"
-            + "<tr><td>"+i18n( "Early progress" )+"</td><td bgcolor=\""
+            + "<tr><td>"+i18n("Early progress")+"</td><td bgcolor=\""
             + preGradeColor().name()+"\"><nobr>    </nobr></td></tr>"
-            + "<tr><td>"+i18n( "Not Practiced" )+"</td><td bgcolor=\""
+            + "<tr><td>"+i18n("Not Practiced")+"</td><td bgcolor=\""
             + unpracticedColor().name()+"\"><nobr>    </nobr></td></tr>"
             + "<tr><td>"+i18n("Invalid Entries")+"</td><td bgcolor=\""
             + invalidColor().name()+"\" width=\"15%\"><nobr>    </nobr></td></tr></table>";
-    setToolTip(ttip );
+    setToolTip(ttip);
 }
 
-QColor StatisticsLegendWidget::gradeColor(int grade) {
-    QColor color( Prefs::gradeColor() );
-    color.setAlpha( alphaMax - ( KV_MAX_GRADE - grade - 1) * alphaStep );
+QColor StatisticsLegendWidget::gradeColor(int grade)
+{
+    QColor color(Prefs::gradeColor());
+    color.setAlpha(alphaMax - (KV_MAX_GRADE - grade - 1) * alphaStep);
     return color;
 }
 
-QColor StatisticsLegendWidget::preGradeColor() {
-    QColor color( Prefs::preGradeColor() );
-    color.setAlpha( alphaMax );
+QColor StatisticsLegendWidget::preGradeColor()
+{
+    QColor color(Prefs::preGradeColor());
+    color.setAlpha(alphaMax);
     return color;
 }
 
-QColor StatisticsLegendWidget::unpracticedColor() {
-    QColor color( "#FFFFFF" );
-    color.setAlpha( alphaMax );
+QColor StatisticsLegendWidget::unpracticedColor()
+{
+    QColor color("#FFFFFF");
+    color.setAlpha(alphaMax);
     return color;
 }
 
-QColor StatisticsLegendWidget::invalidColor() {
+QColor StatisticsLegendWidget::invalidColor()
+{
     return Prefs::invalidUnitColor();
 }
 
 QList< QVariant > StatisticsLegendWidget::legendFractions(KEduVocContainer & container,  int index)
 {
-    QVector<double> sums( KV_MAX_GRADE + fractionsOffset + 1, 0);
-    double count( 0 );
-    foreach (KEduVocExpression *entry, container.entries( KEduVocContainer::Recursive ) ) {
-        KEduVocTranslation & trans( *entry->translation(index) );
+    QVector<double> sums(KV_MAX_GRADE + fractionsOffset + 1, 0);
+    double count(0);
+    foreach (KEduVocExpression *entry, container.entries(KEduVocContainer::Recursive)) {
+        KEduVocTranslation & trans(*entry->translation(index));
         ++count;
-        if ( !trans.isEmpty() ) {
-            if ( trans.grade() != 0 ) {
+        if (!trans.isEmpty()) {
+            if (trans.grade() != 0) {
                 sums[trans.grade() + fractionsOffset] += 1;
             } else {
-                if ( trans.preGrade() != 0 ) {
+                if (trans.preGrade() != 0) {
                     sums[2] += 1;
                 } else {
                     sums[1] += 1; //unpracticed
                 }
             }
-        }else{
+        } else {
             sums[0] += 1; //invalid
         }
     }
+
     QList< QVariant > fractions;
-    for( int ii =0 ;ii < KV_MAX_GRADE + fractionsOffset; ++ii) {
-        fractions.push_back(( double )( sums[ii] / count ));
+    for (int ii =0 ;ii < KV_MAX_GRADE + fractionsOffset; ++ii) {
+        fractions.push_back((double) (sums[ii] / count));
     }
+
     return fractions;
 }
 
-void StatisticsLegendWidget::paintStatisticsBar(
-    QPainter & painter, const QRect & rect,  const QList< QVariant> &fractions)
+void StatisticsLegendWidget::paintStatisticsBar(QPainter & painter, const QRect & rect,
+						const QList< QVariant> &fractions)
 {
     QRect roundedRect(rect);
     roundedRect.adjust(1, 1, -1, -1);
@@ -103,21 +116,22 @@ void StatisticsLegendWidget::paintStatisticsBar(
 
     QColor color;
     int xPosition = 0;
-    for(int ii = KV_MAX_GRADE + fractionsOffset - 1; ii >= 0; --ii ) {
-        double fraction( fractions[ii].toDouble() );
+    for (int ii = KV_MAX_GRADE + fractionsOffset - 1; ii >= 0; --ii) {
+        double fraction(fractions[ii].toDouble());
         int barElementWidth = fraction * rect.width();
         QRectF barElement(rect.x() + xPosition, rect.y(), barElementWidth, rect.height());
         QPainterPath barElementPath;
         barElementPath.addRect(barElement);
         xPosition += barElementWidth;
+
         // Intersect the QPainterPath of inner rectangle with outer,
         // so that the inner rectangle takes the shape of the outer rounded rectangle.
         QPainterPath barElementIntersectedPath = roundedPath.intersected(barElementPath);
 
-        if ( ii >= fractionsOffset ) {
-            color = StatisticsLegendWidget::gradeColor( ii - fractionsOffset);
+        if (ii >= fractionsOffset) {
+            color = StatisticsLegendWidget::gradeColor(ii - fractionsOffset);
         } else {
-            switch( ii ) {
+            switch (ii) {
             case 2:
                 color = StatisticsLegendWidget::preGradeColor();
                 break;
@@ -134,7 +148,6 @@ void StatisticsLegendWidget::paintStatisticsBar(
         painter.setBrush(QBrush(color));
         painter.drawPath(barElementIntersectedPath);
     }
-
 }
 
 
@@ -153,24 +166,28 @@ void StatisticsLegendWidget::paintEvent(QPaintEvent *)
     QString rightString = i18nc("adjective, The word has not even been practiced once by the user","Not practiced");
     QFontMetrics fontMetrics(painter.font());
 
-    // Calculate the size and position of the rectangle that will contain the string on the left side of the legend
+    // Calculate the size and position of the rectangle that will contain the
+    // string on the left side of the legend.
     QRect leftRect = fontMetrics.boundingRect(leftString);
     leftRect.setWidth(leftRect.width() + extraRoomInRectSize);
     leftRect.setHeight(leftRect.height() + extraRoomInRectSize);
-    leftRect.moveBottomRight(QPoint(legendOffsetX - horizontalDistanceFromLegend, legendOffsetY + legendHeight));
+    leftRect.moveBottomRight(QPoint(legendOffsetX - horizontalDistanceFromLegend,
+				    legendOffsetY + legendHeight));
 
-    // Calculate the size and position of the rectangle that will contain the string on the right side of the legend
+    // Calculate the size and position of the rectangle that will contain the
+    // string on the right side of the legend.
     QRect rightRect = fontMetrics.boundingRect(rightString);
     rightRect.setWidth(rightRect.width() + extraRoomInRectSize);
     rightRect.setHeight(rightRect.height() + extraRoomInRectSize);
-    rightRect.moveBottomLeft(QPoint(legendOffsetX + legendWidth + horizontalDistanceFromLegend, legendOffsetY + legendHeight));
+    rightRect.moveBottomLeft(QPoint(legendOffsetX + legendWidth + horizontalDistanceFromLegend,
+				    legendOffsetY + legendHeight));
 
     QRect rect(x() + legendOffsetX, legendOffsetY, legendWidth, legendHeight);
     QList<QVariant> fractions(
         QList<QVariant>::fromVector(
             QVector<QVariant>(KV_MAX_GRADE + fractionsOffset + 1,
-			      (double) 1.0/(KV_MAX_GRADE+fractionsOffset) ) ) );
-    paintStatisticsBar( painter, rect, fractions);
+			      (double) 1.0 / (KV_MAX_GRADE + fractionsOffset))));
+    paintStatisticsBar(painter, rect, fractions);
 
     painter.setPen(Qt::black);
     painter.drawText(leftRect, Qt::AlignRight | Qt::AlignVCenter, leftString);
