266043d02872955d1e720cdaa232987099529afb
Andreas Xavier <andxav@zoho.com>
2014-08-24 13:05:13 -0400

    Changed LGPL to GPL license erroneously copied from library

--- src/statistics/statisticslegendwidget.h
+++ src/statistics/statisticslegendwidget.h
@@ -2,21 +2,21 @@
  *  Copyright 2014  Andreas Xavier <andxav at zoho dot com>
  *  Copyright 2014  Inge Wallin
  *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 2.1 of the License, or (at your option) version 3, or any
- *  later version accepted by the membership of KDE e.V. (or its
- *  successor approved by the membership of KDE e.V.), which shall
- *  act as a proxy defined in Section 6 of version 3 of the license.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of
+ * the License or any later version accepted by the membership of
+ * KDE e.V. (or its successor approved by the membership of KDE
+ * e.V.), which shall act as a proxy defined in Section 14 of
+ * version 3 of the license.
  *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef STATISTICSLEGENDWIDGET_H
@@ -61,7 +61,7 @@ public:
      @param fractions the fractional widths of each of the grade categories
     **/
     static void paintStatisticsBar(QPainter &painter, const QRect &rect,
-				   const QList< QVariant> &fractions);
+                                   const QList< QVariant> &fractions);
 
 protected:
     /// paintEvent @p e event
