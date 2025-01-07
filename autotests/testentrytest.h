0c9cc2d94dccca10f60595d1d84e305685d64f77
Johnny Jazeix <jazeix@gmail.com>
2025-01-07 20:48:23 +0100

    use Q_SLOTS instead of slots

--- autotests/testentrytest.h
+++ autotests/testentrytest.h
@@ -22,7 +22,7 @@ class TestEntryTest : public QObject
 {
     Q_OBJECT
 
-private slots:
+private Q_SLOTS:
     void initTestCase();
     void cleanupTestCase();
     void test_practiceModeDependentMinGrade_written();
